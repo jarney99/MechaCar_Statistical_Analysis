c2f388a9382bc3f60ba2f3b3929b0c5e8f74c640
jarney99 <95194554+jarney99@users.noreply.github.com>
2022-04-07 10:45:57 -0500

    deliverable 3 done

--- 01_Demo/MechaCarChallenge.RScript.R
+++ 01_Demo/MechaCarChallenge.RScript.R
@@ -24,3 +24,11 @@ SD = sd(Coil_Data$PSI)
 total_Summary <- data.frame(Mean,Median,Variance,SD)
 # Lot Summary
 lot_summary <- Coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
+
+# Deliverable 3
+# T-test across all manufacturing lots
+t.test((Coil_Data$PSI),mu = 1500)
+# T-test for each lot
+t.test(subset(Coil_Data,Manufacturing_Lot =="Lot1")$PSI,mu = 1500)
+t.test(subset(Coil_Data,Manufacturing_Lot =="Lot2")$PSI,mu = 1500)
+t.test(subset(Coil_Data,Manufacturing_Lot =="Lot3")$PSI,mu = 1500)
