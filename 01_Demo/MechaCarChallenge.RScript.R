a1d74a3486c69911e4cc81a10b4b47fcb25a0db1
jarney99 <95194554+jarney99@users.noreply.github.com>
2022-04-07 10:43:46 -0500

    deliverable 2 done

--- 01_Demo/MechaCarChallenge.RScript.R
+++ 01_Demo/MechaCarChallenge.RScript.R
@@ -1,3 +1,4 @@
+#Deliverable 1
 # Load Packages
 library(dplyr)
 library(tidyverse)
@@ -9,3 +10,17 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 View(Car_Data)
 # Determine P-value and R squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =Car_Data)) 
+
+# Deliverable 2
+# Import and read data
+Coil_Data <- read.csv('Suspension_Coil.csv')
+head(Coil_Data)
+# Mean, Median, Variance, and SD
+Mean = mean(Coil_Data$PSI)
+Median=median(Coil_Data$PSI)
+Variance=var(Coil_Data$PSI)
+SD = sd(Coil_Data$PSI)
+# Total Summary data frame
+total_Summary <- data.frame(Mean,Median,Variance,SD)
+# Lot Summary
+lot_summary <- Coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
